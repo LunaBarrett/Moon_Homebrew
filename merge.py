f7364a02d98585179b7947e10fb21959117a6cc5
LunaBarrett <luna.barrett@stfc.ac.uk>
2024-07-22 12:18:25 +0100

    Huge reorganisation of files

--- merge.py
+++ merge.py
@@ -4,13 +4,17 @@ import time
 import pathlib
 from datetime import datetime
 
+
+def prepare_comparison_source(source):
+    """Return a version of the source without the version attribute, in dictionary form, for comparison."""
+    return json.dumps({k:v for k,v in source.items() if k != 'version'}, sort_keys=True)
+
 def build_json(files):
     output = {}
     for f in files:
 
-        # Get time file was last modifed, to be used as version
-        file_modified_time = pathlib.Path(f).stat().st_mtime
-        version = datetime.utcfromtimestamp(file_modified_time).strftime('%Y.%m.%d.%H.%M')
+        # Use current time as version, as there are no longer duplicate sources
+        version = datetime.utcnow().strftime('%Y.%m.%d.%H.%M')
 
         # Build merged json from all given source jsons, extending rather than replacing where needed
         with open(f, 'r', encoding="ascii", errors="replace") as infile:
@@ -22,7 +26,17 @@ def build_json(files):
                     elif k == "_meta":
                         for source in data[k]["sources"]:
                             source["version"] = version
-                        output[k]["sources"].extend(data[k]["sources"])
+
+                        comp_new_sources = [prepare_comparison_source(source) for source in data[k]["sources"]]
+                        comp_output_sources = [prepare_comparison_source(source) for source in output[k]["sources"]]
+
+                        for new_source, original_source in zip(comp_new_sources, data[k]["sources"]):
+                            if new_source not in comp_output_sources:
+                                print("Adding new source")
+                                output[k]["sources"].append(original_source)
+                            else:
+                                print("rejecting duplicate source")
+
                     else:
                         output[k].extend(data[k])
                 else:
@@ -36,25 +50,52 @@ def write_to_file(filename, data):
     with open(filename, 'w') as output_file:
         json.dump(data, output_file, indent=4)
 
+
 # Get file lists
-# Conditions = glob.glob("Source/Conditions/All_Conditions.json")
-Items = glob.glob("Source/Items/All_Items.json")
-# Monsters = glob.glob("Source/Monsters/All_Monsters.json")
-# Backgrounds = glob.glob("Source/Player_Centric/Backgrounds/All_Backgrounds.json")
-# Classes = glob.glob("Source/Player_Centric/Classes/All_Classes.json")
-# Feats = glob.glob("Source/Player_Centric/Feats/All_Feats.json")
-Races = glob.glob("Source/Races/All_Races.json")
-Spells = glob.glob("Source/Spells/All_Spells.json")
-# All_Homebrew = Conditions + Items + Monsters + Backgrounds + Classes + Feats + Races + Spells
-All_Homebrew = Items + Races + Spells
+
+# Elebus Campaign Sources
+
+E_Backgrounds = glob.glob("Source\Elebus\Elebus_Backgrounds.json")
+E_Classes = glob.glob("Source\Elebus\Elebus_Classes.json")
+E_Conditions = glob.glob("Source\Elebus\Elebus_Conditions.json")
+E_Feats = glob.glob("Source\Elebus\Elebus_Feats.json")
+E_Items = glob.glob("Source\Elebus\Elebus_Items.json")
+E_Monsters = glob.glob("Source\Elebus\Elebus_Monsters.json")
+E_Races = glob.glob("Source\Elebus\Elebus_Races.json")
+E_Spells = glob.glob("Source\Elebus\Elebus_Spells.json")
+E_Variant_Rules = glob.glob("Source\Elebus\Elebus_Variant_Rules.json")
+E_Homebrew = E_Backgrounds + E_Classes + E_Conditions + E_Feats + E_Items + E_Monsters + E_Races + E_Spells + E_Variant_Rules
+
+# General Sources
+
+Gen_Backgrounds = glob.glob("Source\General\General_Backgrounds.json")
+Gen_Classes = glob.glob("Source\General\General_Classes.json")
+Gen_Conditions = glob.glob("Source\General\General_Conditions.json")
+Gen_Feats = glob.glob("Source\General\General_Feats.json")
+Gen_Items = glob.glob("Source\General\General_Items.json")
+Gen_Monsters = glob.glob("Source\General\General_Monsters.json")
+Gen_Races = glob.glob("Source\General\General_Races.json")
+Gen_Spells = glob.glob("Source\General\General_Spells.json")
+Gen_Variant_Rules = glob.glob("Source\General\General_Variant_Rules.json")
+Gen_Homebrew = Gen_Backgrounds + Gen_Classes + Gen_Conditions + Gen_Feats + Gen_Items + Gen_Monsters + Gen_Races + Gen_Spells + Gen_Variant_Rules
+
+# Spelljammer Campaign Sources
+
+S_Backgrounds = glob.glob("Source\Spelljammer\Spelljammer_Backgrounds.json")
+S_Classes = glob.glob("Source\Spelljammer\Spelljammer_Classes.json")
+S_Conditions = glob.glob("Source\Spelljammer\Spelljammer_Conditions.json")
+S_Feats = glob.glob("Source\Spelljammer\Spelljammer_Feats.json")
+S_Items = glob.glob("Source\Spelljammer\Spelljammer_Items.json")
+S_Monsters = glob.glob("Source\Spelljammer\Spelljammer_Monsters.json")
+S_Races = glob.glob("Source\Spelljammer\Spelljammer_Races.json")
+S_Spells = glob.glob("Source\Spelljammer\Spelljammer_Spells.json")
+S_Variant_Rules = glob.glob("Source\Spelljammer\Spelljammer_Variant_Rules.json")
+S_Homebrew = S_Backgrounds + S_Classes + S_Conditions + S_Feats + S_Items + S_Monsters + S_Races + S_Spells + S_Variant_Rules
+
 
 # Get data and write
-# write_to_file("Merged/Moon_Conditions.json", build_json(Conditions))
-# write_to_file("Merged/Moon_Items.json", build_json(Items))
-# write_to_file("Merged/Moon_Monsters.json", build_json(Monsters))
-# write_to_file("Merged/Moon_Backgrounds.json", build_json(Backgrounds))
-# write_to_file("Merged/Moon_Classes.json", build_json(Classes))
-# write_to_file("Merged/Moon_Feats.json", build_json(Feats))
-# write_to_file("Merged/Moon_Races.json", build_json(Races))
-# write_to_file("Merged/Moon_Spells.json", build_json(Spells))
+All_Homebrew = Gen_Homebrew + S_Homebrew + E_Homebrew
+write_to_file("Merged\Moon_Elebus_Homebrew.json", build_json(E_Homebrew))
+write_to_file("Merged\Moon_General_Homebrew.json", build_json(Gen_Homebrew))
+write_to_file("Merged\Moon_Spelljammer_Homebrew.json", build_json(S_Homebrew))
 write_to_file("Merged/Moon_All_Homebrew.json", build_json(All_Homebrew))
